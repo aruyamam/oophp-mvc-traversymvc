dcbe5f036c13f45066b1a5450e69a6f97d8880ea
aruyamam <aruyamam@gmail.com>
2019-03-08 10:55:35 +0900

    Mapping Methods & Parameters

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -25,6 +25,20 @@ class Core
       require_once '../app/controllers/' . $this->currentController . '.php';
 
       $this->currentController = new $this->currentController;
+
+      // Check for second part of url
+      if (isset($url[1])) {
+         if (method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+         }
+         unset($url[1]);
+      }
+
+      // Get param
+      $this->params = $url ? array_values($url) : [];
+
+      // Call a callback with array of param
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
    }
 
    public function getUrl()
